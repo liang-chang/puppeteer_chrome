8dc8f8189c1684be635ab7e588dc543f748b14b4
zhangliang <zl_xtyp@163.com>
2019-08-20 00:31:00 +0800

    更新

--- download_geekbang.js
+++ download_geekbang.js
@@ -1,4 +1,5 @@
 const puppeteer = require('puppeteer-core');
+var querystring = require('querystring');
 var http = require('http');
 var https = require('https');
 var querystring = require('querystring');
@@ -6,6 +7,10 @@ var querystring = require('querystring');
 
 //---------------------------------------------------------------
 //配置
+
+var CELL_PHONE="";
+
+var PASSWORD="";
 
 var CHROME_PATH = "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe";
 
@@ -17,63 +22,83 @@ var USER_AGENT = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML,
 
 var cookie = null;
 
-
-
-async function login() {
+function login() {
 
 	var url = "https://account.geekbang.org/account/ticket/login";
 	
+	var post_data = JSON.stringify({
+			"country": 86,
+			"cellphone": CELL_PHONE,
+			"password": PASSWORD,
+			"captcha": "",
+			"remember": 1,
+			"platform": 3,
+			"appid": 1
+		});
+		
 	var options = {
-		Host: 'account.geekbang.org',
+		host: 'account.geekbang.org',
+		hostname :'account.geekbang.org',
+		port: 443,
+		path: '/account/ticket/login',
 		method: 'POST',
-		agent:USER_AGENT,
 		headers: {
 			'User-Agent': USER_AGENT,
             'Accept': 'application/json, text/plain, */*',
-            'Accept-Language': 'zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2',
+            'Accept-Language': 'zh-CN,zh;q=0.9',
             'Host': 'account.geekbang.org',
-            'Referer': 'https://account.geekbang.org/signin?redirect=https%3A%2F%2Fwww.geekbang.org%2F',
+			'Origin':'https://account.geekbang.org',
+            'Referer': 'https://account.geekbang.org/login?redirect=https%3A%2F%2Ftime.geekbang.org%2F',
 		}
 	}
 
-	var req = https.request(url,options,(res) => {
+	var req = https.request(options,(res) => {
 			res.setEncoding('utf8');
 			res.on('data', function (data) {
 				console.log("data:", data); //一段html代码
 			});
+			//console.log(res.getHeaders());
 			let setCookie = res.getHeader('set-cookie');
 			console.log(setCookie);
 		});
+		
+	req.on('error', function(e) {
+		console.log('problem with request: ' + e.message);
+	});
+	
+	req.write(post_data);
+	req.end();
 }
 
 login(); 
 
-//function ttt(){
-//	const browser = await puppeteer.launch({
-//		headless: HEADLESS,
-//		executablePath: CHROME_PATH
-//	});
-//	
-//	const page = await browser.newPage();
-//
-//	await page.setExtraHTTPHeaders({
-//		'Host': 'account.geekbang.org',
-//		referer: 'https://time.geekbang.org/'
-//	});
-//
-//	await page.goto('https://account.geekbang.org/login?redirect=https%3A%2F%2Ftime.geekbang.org%2F', {
-//		waitUntil: 'networkidle2',
-//		referer: 'https://time.geekbang.org/'
-//	});
-//
-//	await page.pdf({
-//		path: 'hn.pdf',
-//		format: 'A4'
-//	});
-//
-//	await browser.close();
-//	
-//}
+/* 
+function ttt(){
+	const browser = await puppeteer.launch({
+		headless: HEADLESS,
+		executablePath: CHROME_PATH
+	});
+	
+	const page = await browser.newPage();
+
+	await page.setExtraHTTPHeaders({
+		'Host': 'account.geekbang.org',
+		referer: 'https://time.geekbang.org/'
+	});
+
+	await page.goto('https://account.geekbang.org/login?redirect=https%3A%2F%2Ftime.geekbang.org%2F', {
+		waitUntil: 'networkidle2',
+		referer: 'https://time.geekbang.org/'
+	});
+
+	await page.pdf({
+		path: 'hn.pdf',
+		format: 'A4'
+	});
+
+	await browser.close();	
+}
+*/
 
 (async() => {
 	
